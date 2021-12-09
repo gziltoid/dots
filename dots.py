8694fb52a34c9e884d18f3659661c8159bd66853
gziltoid <g.ziltoid@gmail.com>
2021-12-09 21:10:36 +0100

    Move a circle at a random angle

--- dots.py
+++ dots.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from graphics import *
 from random import randint
+from math import sin, cos
 
 WIDTH, HEIGHT = 800, 600
 CIRCLE_RADIUS = 7
@@ -16,14 +17,17 @@ def main():
     message.setSize(18)
     message.draw(win)
 
-    x, y = randint(6, WIDTH - 1 - CIRCLE_RADIUS), randint(6, HEIGHT - 1 - CIRCLE_RADIUS)
-    c = Circle(Point(x, y), CIRCLE_RADIUS)
+    x0, y0 = randint(6, WIDTH - 1 - CIRCLE_RADIUS), randint(6,
+                                                            HEIGHT - 1 - CIRCLE_RADIUS)
+    c = Circle(Point(x0, y0), CIRCLE_RADIUS)
     c.setFill("white")
     c.draw(win)
 
+    x, y = randint(-3, 3), randint(-3, 3)
+
     while not win.checkKey():
-        c.move(SPEED, 0)
-        time.sleep(.2)
+        c.move(x, y)
+        time.sleep(.03)
 
     win.close()
 
