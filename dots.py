d21fa7e7428fd9dc15a4acd687619f61c2b1d019
gziltoid <g.ziltoid@gmail.com>
2021-12-09 19:49:50 +0100

    Create a random circle and move it to the right every 20 ms

--- dots.py
+++ dots.py
@@ -4,6 +4,7 @@ from random import randint
 
 WIDTH, HEIGHT = 800, 600
 CIRCLE_RADIUS = 7
+SPEED = 10
 
 
 def main():
@@ -12,15 +13,17 @@ def main():
 
     message = Text(Point(win.getWidth() / 2, 20), "Press any key to quit.")
     message.setTextColor("pink")
+    message.setSize(18)
     message.draw(win)
 
+    x, y = randint(6, WIDTH - 1 - CIRCLE_RADIUS), randint(6, HEIGHT - 1 - CIRCLE_RADIUS)
+    c = Circle(Point(x, y), CIRCLE_RADIUS)
+    c.setFill("white")
+    c.draw(win)
+
     while not win.checkKey():
-        x, y = randint(6, WIDTH - 1 - CIRCLE_RADIUS), randint(6, HEIGHT - 1 - CIRCLE_RADIUS)
-        c = Circle(Point(x, y), CIRCLE_RADIUS)
-        c.setFill("white")
-        c.draw(win)
-        time.sleep(0.2)
-        c.undraw()
+        c.move(SPEED, 0)
+        time.sleep(.2)
 
     win.close()
 
