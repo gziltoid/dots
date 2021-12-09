b523be2bb4aa44e5aba7a1fc00ea26c9bc09d6cc
gziltoid <g.ziltoid@gmail.com>
2021-12-09 19:35:17 +0100

    Draw random white circle every 20ms

--- dots.py
+++ dots.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
-from turtle import Screen, Turtle
-
+from graphics import *
+from random import randint
 
 WIDTH, HEIGHT = 800, 600
+CIRCLE_RADIUS = 7
 
-screen = Screen()
-screen.setup(WIDTH, HEIGHT)
-screen.bgcolor('black')
 
-turtle = Turtle()
+def main():
+    win = GraphWin("Dots", WIDTH, HEIGHT)
+    win.setBackground("black")
 
-screen.exitonclick()
+    message = Text(Point(win.getWidth() / 2, 20), "Press any key to quit.")
+    message.setTextColor("pink")
+    message.draw(win)
+
+    while not win.checkKey():
+        x, y = randint(6, WIDTH - 1 - CIRCLE_RADIUS), randint(6, HEIGHT - 1 - CIRCLE_RADIUS)
+        c = Circle(Point(x, y), CIRCLE_RADIUS)
+        c.setFill("white")
+        c.draw(win)
+        time.sleep(0.2)
+        c.undraw()
+
+    win.close()
+
+
+main()
