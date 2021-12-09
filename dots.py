dad2ebd7a55fc704bc86a1c5ec7953fa1b2affb2
gziltoid <g.ziltoid@gmail.com>
2021-12-09 16:32:42 +0100

    First commit

---
+++ dots.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from turtle import Screen, Turtle
+
+
+WIDTH, HEIGHT = 800, 600
+
+screen = Screen()
+screen.setup(WIDTH, HEIGHT)
+screen.bgcolor('black')
+
+turtle = Turtle()
+
+screen.exitonclick()
