d66c4657a62e058cbba9d7a6f9542348d3763b34
gziltoid <g.ziltoid@gmail.com>
2021-12-15 00:49:55 +0100

    Draw a line beetween 2 circles with the distance less than 80

--- dots.py
+++ dots.py
@@ -4,45 +4,65 @@ from graphics import *
 from random import randrange, uniform
 
 
-WIDTH, HEIGHT = 800, 600
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 CIRCLE_RADIUS = 6
-CIRCLE_NUMBERS = 80
-SPEED = 0.6
+CIRCLES_NUMBER = 80
+CIRCLE_SPEED = 0.6
+DISTANCE_LIMIT = 80
 
 
 def main():
-    win = GraphWin("Dots", WIDTH, HEIGHT, autoflush=False)
+    win = GraphWin("Dots", SCREEN_WIDTH, SCREEN_HEIGHT, autoflush=False)
     win.setBackground("black")
 
-    message = Text(Point(win.getWidth() / 2, 20), "Click anywhere to quit.")
+    message = Text(Point(win.getWidth() / 2, 20), "Click anywhere to quit")
     message.setTextColor("pink")
     message.setSize(18)
     message.draw(win)
 
     circles = []
-    for _ in range(CIRCLE_NUMBERS):
-        x0, y0 = randrange(WIDTH), randrange(HEIGHT)
+    lines = []
+    for _ in range(CIRCLES_NUMBER):
+        x0, y0 = randrange(SCREEN_WIDTH), randrange(SCREEN_HEIGHT)
         c = Circle(Point(x0, y0), CIRCLE_RADIUS)
         angle = uniform(0, 2 * pi)
-        dx = SPEED * cos(angle)
-        dy = SPEED * sin(angle)
+        dx = CIRCLE_SPEED * cos(angle)
+        dy = CIRCLE_SPEED * sin(angle)
         c.setFill("white")
         c.draw(win)
         circles.append((c, dx, dy))
 
     while not win.checkMouse():
+        for line in lines:
+            line.undraw()
+
         for i, (c, dx, dy) in enumerate(circles):
             c.undraw()
             centerPoint = c.getCenter()
             curr_x, curr_y = centerPoint.x, centerPoint.y
 
-            x = (curr_x + dx) % WIDTH
-            y = (curr_y + dy) % HEIGHT
+            x = (curr_x + dx) % SCREEN_WIDTH
+            y = (curr_y + dy) % SCREEN_HEIGHT
 
             c = Circle(Point(x, y), CIRCLE_RADIUS)
+            circles[i] = (c, dx, dy)
+
+        for i, (c1, _, _) in enumerate(circles):
+            centerPoint = c1.getCenter()
+            x1, y1 = centerPoint.x, centerPoint.y
+            for c2, _, _ in circles[i + 1 :]:
+                centerPoint2 = c2.getCenter()
+                x2, y2 = centerPoint2.x, centerPoint2.y
+                d = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+                if d < DISTANCE_LIMIT:
+                    line = Line(Point(x1, y1), Point(x2, y2))
+                    lines.append(line)
+                    line.setFill("green")
+                    line.draw(win)
+
+        for c, _, _ in circles:
             c.setFill("white")
             c.draw(win)
-            circles[i] = (c, dx, dy)
 
         update(30)
 
