913a1c9a3be1baa51631a589a285d4b476df3597
gziltoid <g.ziltoid@gmail.com>
2021-12-14 01:40:36 +0100

    Draw 80 random circles each moving at a random angle

--- dots.py
+++ dots.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
+from math import cos, pi, sin
 from graphics import *
 from random import randrange, uniform
 
 
 WIDTH, HEIGHT = 800, 600
-CIRCLE_RADIUS = 7
+CIRCLE_RADIUS = 6
+CIRCLE_NUMBERS = 80
+SPEED = 0.6
 
 
 def main():
-    win = GraphWin("Dots", WIDTH, HEIGHT)
+    win = GraphWin("Dots", WIDTH, HEIGHT, autoflush=False)
     win.setBackground("black")
 
     message = Text(Point(win.getWidth() / 2, 20), "Click anywhere to quit.")
@@ -16,30 +19,32 @@ def main():
     message.setSize(18)
     message.draw(win)
 
-    x0, y0 = randrange(WIDTH), randrange(HEIGHT)
-    c = Circle(Point(x0, y0), CIRCLE_RADIUS)
-    c.setFill("white")
-    c.draw(win)
-
-    dx, dy = uniform(-3.0, 3.0), uniform(-3.0, 3.0)
-
-    while True:
-        c.undraw()
-        centerPoint = c.getCenter()
-        curr_x, curr_y = centerPoint.x, centerPoint.y
-
-        x = (curr_x + dx) % WIDTH
-        y = (curr_y + dy) % HEIGHT
-
-        c = Circle(Point(x, y), CIRCLE_RADIUS)
+    circles = []
+    for _ in range(CIRCLE_NUMBERS):
+        x0, y0 = randrange(WIDTH), randrange(HEIGHT)
+        c = Circle(Point(x0, y0), CIRCLE_RADIUS)
+        angle = uniform(0, 2 * pi)
+        dx = SPEED * cos(angle)
+        dy = SPEED * sin(angle)
         c.setFill("white")
         c.draw(win)
+        circles.append((c, dx, dy))
 
-        time.sleep(0.015)
+    while not win.checkMouse():
+        for i, (c, dx, dy) in enumerate(circles):
+            c.undraw()
+            centerPoint = c.getCenter()
+            curr_x, curr_y = centerPoint.x, centerPoint.y
 
-        if win.checkMouse():
-            win.close()
-            break
+            x = (curr_x + dx) % WIDTH
+            y = (curr_y + dy) % HEIGHT
+
+            c = Circle(Point(x, y), CIRCLE_RADIUS)
+            c.setFill("white")
+            c.draw(win)
+            circles[i] = (c, dx, dy)
+
+        update(30)
 
 
 if __name__ == "__main__":
