00a53f4481ab98d01822bbcbc0aba40d6d662789
gziltoid <g.ziltoid@gmail.com>
2021-12-17 19:12:50 +0100

    Save canvas animation as GIF using PIL

--- dots.py
+++ dots.py
@@ -7,28 +7,38 @@ from random import randrange, uniform
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 CIRCLE_RADIUS = 6
 CIRCLES_NUMBER = 80
-CIRCLE_SPEED = 0.6
+CIRCLE_SPEED = 0.5
 DISTANCE_LIMIT = 80
+
+img_list = []
 
 
 def main():
-    win = GraphWin("Dots", SCREEN_WIDTH, SCREEN_HEIGHT, autoflush=False)
-    win.setBackground("black")
+    win = GraphWin('Dots', SCREEN_WIDTH, SCREEN_HEIGHT, autoflush=False)
 
-    message = Text(Point(win.getWidth() / 2, 20), "Click anywhere to quit")
-    message.setTextColor("pink")
-    message.setSize(18)
+    rectangle = Rectangle(
+        Point(0, 0),
+        Point(SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1))
+    rectangle.setFill('black')
+    rectangle.draw(win)
+
+    message = Text(Point(win.getWidth() / 2, 20), 'Click anywhere to quit')
+    message.setTextColor('pink')
+    message.setFace('courier')
+    message.setSize(16)
     message.draw(win)
 
     circles = []
     lines = []
+    n = 0
+
     for _ in range(CIRCLES_NUMBER):
         x0, y0 = randrange(SCREEN_WIDTH), randrange(SCREEN_HEIGHT)
         c = Circle(Point(x0, y0), CIRCLE_RADIUS)
         angle = uniform(0, 2 * pi)
         dx = CIRCLE_SPEED * cos(angle)
         dy = CIRCLE_SPEED * sin(angle)
-        c.setFill("white")
+        c.setFill('white')
         c.draw(win)
         circles.append((c, dx, dy))
 
@@ -50,22 +60,44 @@ def main():
         for i, (c1, _, _) in enumerate(circles):
             centerPoint = c1.getCenter()
             x1, y1 = centerPoint.x, centerPoint.y
-            for c2, _, _ in circles[i + 1 :]:
+            for c2, _, _ in circles[i + 1:]:
                 centerPoint2 = c2.getCenter()
                 x2, y2 = centerPoint2.x, centerPoint2.y
                 d = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
                 if d < DISTANCE_LIMIT:
                     line = Line(Point(x1, y1), Point(x2, y2))
                     lines.append(line)
-                    line.setFill("green")
+                    line.setFill('green')
                     line.draw(win)
 
         for c, _, _ in circles:
-            c.setFill("white")
+            c.setFill('white')
             c.draw(win)
 
         update(30)
 
+        # save the current TKinter object to postscript format
+        win.postscript(file=f"{n}.eps", colormode='color')
+        n += 1
 
-if __name__ == "__main__":
+    save_as_gif(n)
+
+
+def save_as_gif(n):
+    '''Convert from eps to gif format using PIL'''
+    from PIL import Image
+    import subprocess
+
+    images = []
+    for i in range(n):
+        im = Image.open(f'{i}.eps')
+        im.save(f'{i}.png')
+        images.append(im)
+
+    images[0].save('dots.gif', save_all=True, append_images=images[1:], loop=0)
+
+    subprocess.call(' '.join(['rm', './*.png', './*.eps']), shell=True)
+
+
+if __name__ == '__main__':
     main()
